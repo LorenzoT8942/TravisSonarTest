0411fa6fba9a0ebccd42613e5261889b9aa740c9
LorenzoT8942 <56634954+LorenzoT8942@users.noreply.github.com>
2021-02-18 13:46:16 +0100

    -first commit

---
+++ GithubActionsTest/src/logic/Helo.java
@@ -0,0 +1,16 @@
+package logic;
+
+public class Helo {
+	
+	public Helo (String name) {
+		System.out.println("Helo, " + name + "!");
+		System.out.println("GithubActionsTest");
+	}
+
+	public printCoseBelle(){
+		String cose = "cose";
+		String belle = "belle";
+		String cosebelle = cose + " " + belle;
+		System.out.println(cosebelle);
+	}
+}
